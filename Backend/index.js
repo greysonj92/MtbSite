50727aed5b50d32bc765d4ede18a1ea813110964
Greyson Jones <rgreysonjones@gmail.com>
2023-11-30 11:12:20 -0500

    small changes

--- Backend/index.js
+++ Backend/index.js
@@ -5,6 +5,26 @@ import { Ride } from './models/rideModel.js'
 
 const app = express();
 
-app.listen(PORT, () => {
+// Middleware for parsing request body
+app.use(express.json());
 
-})
+// HTTP GET '/', just a test that this works
+app.get('/', (request, response) => {
+    console.log(request);
+    return response.status(234).send('/ Works!');
+});
+
+// TODO: implement this router
+app.use('/rides', ridesRoute);
+mongoose
+    // TODO: add the mongoDBURL constant to config.js
+    .connect(mongoDBURL)
+    .then(() => {
+        console.log("App connected to remote MongoDB")
+        app.listen(PORT, () => {
+            console.log(`App is listening on port ${PORT}`);
+        });
+    })
+    .catch((error) => {
+        console.log(error);
+    });
