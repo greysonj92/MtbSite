55d0a7d907699137ee1c65eb293a36b36d21a9e8
Greyson Jones <rgreysonjones@gmail.com>
2023-12-02 16:07:14 -0500

    Added RecentRides page

---
+++ frontend/src/pages/RecentRides.jsx
@@ -0,0 +1,11 @@
+import React, { useState, useEffect} from 'react'
+import axios from 'axios';
+
+
+const RecentRides = () => {
+  return (
+    <div>RecentRides</div>
+  )
+}
+
+export default RecentRides
