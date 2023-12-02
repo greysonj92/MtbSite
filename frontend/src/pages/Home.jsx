fc2be5678136359010f242540a32c54165728a6c
Greyson Jones <rgreysonjones@gmail.com>
2023-12-02 14:52:39 -0500

    POC home page working on front end

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -7,11 +7,49 @@ const Home = () => {
     const [loading, setLoading] = useState(false);
     useEffect(() => {
         setLoading(true);
-        axios.get('http://localhost:5555/books/lastfive')
-    })
-  return (
-    <div><h1>Home</h1></div>
-  )
-}
+        axios.get('http://localhost:5555/rides/last/5')
+        .then((response) => {
+            setRides(response.data.data);
+            setLoading(false);
+        })
+        .catch((error) => {
+            console.log(error);
+            setLoading(false);
+        });
+    }, []);
+    return (
+    <div>
+        <h1>Five Most Recent Rides</h1>
+        <table >
+            <thead>
+                <tr>
+                    <th>Rider Name</th>
+                    <th>Trail Name</th>
+                    <th>Trail Difficulty</th>
+                    <th>Ride Date</th>
+                </tr>
+            </thead>
+            <tbody>
+                {rides.map((ride, index) => (
+                    <tr key={ride._id}>
+                        <td>
+                            {ride.riderName}
+                        </td>
+                        <td>
+                            {ride.trailName}
+                        </td>
+                        <td>
+                            {ride.trailDifficulty}
+                        </td>
+                        <td>
+                            {ride.rideDate}
+                        </td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+    </div>
+  );
+};
 
 export default Home
